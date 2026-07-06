5d478e3357914aea2d2eca128a7a6a77865e3230
Falagh Sepehr <falaghsepehr6@gmail.com>
2026-07-06 13:20:14 +0330

    refactor: rename symbols file, trim symbol list, and hardcode numeric removal

--- src/InvertedIndexProgram/Source/Services/SimpleTextProcessor.cs
+++ src/InvertedIndexProgram/Source/Services/SimpleTextProcessor.cs
@@ -7,43 +7,49 @@ namespace InvertedIndexProgram;
 /// </summary>
 public class SimpleTextProcessor : ITextProcessor
 {
-    private readonly char[] _symbolsAndNumbers;
-    private readonly HashSet<string> _stopWords;
+  private readonly char[] _symbols;
+  private static readonly char[] _numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  private readonly char[] _allCharsToRemove;
 
-    public SimpleTextProcessor (char[] symbolsAndNumbers, HashSet<string> stopWords)
-    {
-        _symbolsAndNumbers = symbolsAndNumbers;
-        _stopWords = stopWords;
-    }
-    public List<string> ExtractTerms(string rawText)
-    {
-        return NormalizeTerms(PrepareTokens(rawText));
-    }
-    public List<string> PrepareTokens(string rawText)
-    {
-        return Tokenize(rawText.Trim().ToLower());
-    }
-    public List<string> NormalizeTerms(List<string> terms)
-    {
-        return terms
-            .Select(CleanSymbolsAndNumbers)
-            .SelectMany(t => t.Split(' ', StringSplitOptions.RemoveEmptyEntries))
-            .Where(IsIndexable)
-            .Select(Stem)
-            .ToList();
-    }
-    private List<string> Tokenize(string text)
-    {
-        return text.Split([' ', '\t', '\n', '\r'], StringSplitOptions.RemoveEmptyEntries).ToList();
-    }
-    private string CleanSymbolsAndNumbers(string term)
-    {
-        return _symbolsAndNumbers.Aggregate(term, (current, c) => current.Replace(c, ' '));
-    }
-    private bool IsIndexable(string term)
-    {
-        return !_stopWords.Contains(term) && term.Length > 2;
-    }
-    private static readonly EnglishStemmer Stemmer = new();
-    private string Stem(string word) => Stemmer.GetStem(word);
+  private readonly HashSet<string> _stopWords;
+
+  public SimpleTextProcessor(char[] symbols, HashSet<string> stopWords)
+  {
+    _symbols = symbols;
+    _stopWords = stopWords;
+    _allCharsToRemove = new char[_symbols.Length + _numbers.Length];
+    _symbols.CopyTo(_allCharsToRemove, 0);
+    _numbers.CopyTo(_allCharsToRemove, _symbols.Length);
+  }
+  public List<string> ExtractTerms(string rawText)
+  {
+    return NormalizeTerms(PrepareTokens(rawText));
+  }
+  public List<string> PrepareTokens(string rawText)
+  {
+    return Tokenize(rawText.Trim().ToLower());
+  }
+  public List<string> NormalizeTerms(List<string> terms)
+  {
+    return terms
+        .Select(CleanSymbolsAndNumbers)
+        .SelectMany(t => t.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+        .Where(IsIndexable)
+        .Select(Stem)
+        .ToList();
+  }
+  private List<string> Tokenize(string text)
+  {
+    return text.Split([' ', '\t', '\n', '\r'], StringSplitOptions.RemoveEmptyEntries).ToList();
+  }
+  private string CleanSymbolsAndNumbers(string term)
+  {
+    return _allCharsToRemove.Aggregate(term, (current, c) => current.Replace(c, ' '));
+  }
+  private bool IsIndexable(string term)
+  {
+    return !_stopWords.Contains(term) && term.Length > 2;
+  }
+  private static readonly EnglishStemmer Stemmer = new();
+  private string Stem(string word) => Stemmer.GetStem(word);
 }
