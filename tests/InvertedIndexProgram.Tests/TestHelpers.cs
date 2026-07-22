6907945737cc82c4b752351d2e2cc2258ed48209
Falagh Sepehr <falaghsepehr6@gmail.com>
2026-07-22 15:02:40 +0330

    fix: improve dictionary assertion helper to display actual values

--- tests/InvertedIndexProgram.Tests/TestHelpers.cs
+++ tests/InvertedIndexProgram.Tests/TestHelpers.cs
@@ -1,15 +1,13 @@
+using Xunit;
+
 namespace InvertedIndexProgram.Tests;
 
 public static class TestHelpers
 {
-  public static void AssertEqual<TKey, TValue>(IReadOnlyDictionary<TKey, TValue> expected, IReadOnlyDictionary<TKey, TValue> actual)
+  public static void AssertEqual(IReadOnlyDictionary<string, HashSet<string>> expected, IReadOnlyDictionary<string, HashSet<string>> actual)
   {
     var missingInActual = expected.Keys.Except(actual.Keys).ToList();
     var extraInActual = actual.Keys.Except(expected.Keys).ToList();
-    var valueMismatches = expected.Keys
-        .Where(k => actual.ContainsKey(k) && !EqualityComparer<TValue>.Default.Equals(expected[k], actual[k]))
-        .ToList();
-
     var errors = new List<string>();
 
     if (missingInActual.Count > 0)
@@ -18,8 +16,16 @@ public static class TestHelpers
     if (extraInActual.Count > 0)
       errors.Add($"Extra keys: {string.Join(", ", extraInActual)}");
 
-    foreach (var key in valueMismatches)
-      errors.Add($"Key [{key}]: Expected {expected[key]}, Actual {actual[key]}");
+    foreach (var key in expected.Keys)
+    {
+      if (actual.ContainsKey(key))
+      {
+        var expectedValues = string.Join(", ", expected[key].OrderBy(v => v));
+        var actualValues = string.Join(", ", actual[key].OrderBy(v => v));
+        if (expectedValues != actualValues)
+          errors.Add($"Key [{key}]: Expected [{expectedValues}], Actual [{actualValues}]");
+      }
+    }
 
     if (errors.Count > 0)
       Assert.Fail(string.Join("\n", errors));
